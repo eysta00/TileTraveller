e25e4ba38f0bb6c8a58bec826fd4329958d3c7f2
eysta00 <eysi@live.com>
2019-09-17 10:02:54 +0000

    Algorithim written

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,3 +1,11 @@
-#Eysta00
-print("I have a dream.... of Becoming a gangSTAR")
-print("Hello")
+#Eysteinn Örn Jónsson & Úlfur Örn Björnsson
+# 17/9/19 - Assignment 8
+'''
+Birt á skjáinn eru áttinar sem notandi getur farið í
+    Byður notenda um inntak fyrir áttina sem hann vill ferðast í (n, s, w, e).
+        ef notandi slær inn ógilt inntak þá á að koma villa og notandi á að reyna aftur.
+Þegar notandi kemst á endastað þá kemur á skjáinn að notandi hefur unnið og forritið hættir.
+
+    Hver átt hækkar eða lækkar, x eða y gildið sem mun merkja staðsetningu notanda.
+        Þegar x og y eru ákveðin gildi þá mun birta á skjáinn viðeigandi áttir sem notandi má fara í.
+'''
