5959c60ab4984acdecc106a90ed71abb036920e4
eysta00 <eysi@live.com>
2019-09-17 10:58:46 +0000

    Coding update 1

--- TileTraveller.py
+++ TileTraveller.py
@@ -10,50 +10,60 @@ Birt á skjáinn eru áttinar sem notandi getur farið í
         Þegar x og y eru ákveðin gildi þá mun birta á skjáinn viðeigandi áttir sem notandi má fara í.
         (Þar sem sumar staðsetningar þurfa að skila sömu upplýsingum væri gott að gera sameiginlegt fall)
 '''
+#north + 1, south -1
+#east + 3, west -3
 
+direction_input = ""
+position = 1
 
-pos_x = 1
-pos_y = 1
-
-def Only_North():
+def Only_North(direction):
     print("You can travel: (N)orth.")
-    return 1, 3
+    return position + 1 
 
-def North_South():
+def North_South(direction):
     print("You can travel: (N)orth or (S)outh.")
+    if direction == "n":
+        return + 1
+    elif direction == "s"
+        return - 1
 
-def North_East_South():
+def North_East_South(direction):
     print("You can travel: (N)orth or (E)ast or (S)outh.")
 
-def South_West():
+def South_West(direction):
     print("You can travel: (S)outh or (W)est.")
     return 3, 1
 
-def South_East():
+def South_East(direction):
     print("You can travel: (E)ast or (S)outh.")
     return 2, 2
 
-def West_East():
+def West_East(direction):
     print("You can travel: (E)ast or (W)est.")
 
 
 while True:
-    if pos_x == 1 and pos_y == 1:
-        pos_x, pos_y = Only_North()
-    elif pos_x == 1 and pos_y == 2:
-        pos_x, pos_y = North_East_South()
-    elif pos_x == 1 and pos_y == 3:
-        pos_x, pos_y = South_East()
-    elif pos_x == 2 and pos_y == 1:
-        pos_x, pos_y = Only_North()
-    elif pos_x == 2 and pos_y == 2:
-        pos_x, pos_y = South_West()
-    elif pos_x == 2 and pos_y == 3:
-        pos_x, pos_y = West_East()
-    elif pos_x == 3 and pos_y == 2:
-        pos_x, pos_y = North_South()
-    elif pos_x == 3 and pos_y == 3:
-        pos_x, pos_y = South_West()
+
+    direction_input = input("Direction: ").lower()
+    while direction_input == "n", "s", "w", "e":
+        if position == 1:
+            position = Only_North()
+        elif position == 2:
+            position = North_East_South()
+        elif position == 3:
+            position = South_East()
+        elif position == 4:
+            position = Only_North()
+        elif position == 5:
+            position = South_West()
+        elif position == 6:
+            position = West_East()
+        elif position == 8:
+            position = North_South()
+        elif position == 9:
+            position = South_West()
+        else: #position == 7
+            print("Victory!")
+            break
     else:
-        print("Victory!")
-        break
+        print("Invalid direction")
