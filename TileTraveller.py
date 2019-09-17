d504de4fac52f79a6d034a3352dcd1b337d54586
Ulfur_Orn <ulfurinn@gmail.com>
2019-09-17 10:27:21 +0000

    Skeleton of program

--- TileTraveller.py
+++ TileTraveller.py
@@ -10,3 +10,50 @@ Birt á skjáinn eru áttinar sem notandi getur farið í
         Þegar x og y eru ákveðin gildi þá mun birta á skjáinn viðeigandi áttir sem notandi má fara í.
         (Þar sem sumar staðsetningar þurfa að skila sömu upplýsingum væri gott að gera sameiginlegt fall)
 '''
+
+
+pos_x = 1
+pos_y = 1
+
+def Only_North():
+    print("You can travel: (N)orth.")
+    return 1, 3
+
+def North_South():
+    print("You can travel: (N)orth or (S)outh.")
+
+def North_East_South():
+    print("You can travel: (N)orth or (E)ast or (S)outh.")
+
+def South_West():
+    print("You can travel: (S)outh or (W)est.")
+    return 3, 1
+
+def South_East():
+    print("You can travel: (E)ast or (S)outh.")
+    return 2, 2
+
+def West_East():
+    print("You can travel: (E)ast or (W)est.")
+
+
+while True:
+    if pos_x == 1 and pos_y == 1:
+        pos_x, pos_y = Only_North()
+    elif pos_x == 1 and pos_y == 2:
+        pos_x, pos_y = North_East_South()
+    elif pos_x == 1 and pos_y == 3:
+        pos_x, pos_y = South_East()
+    elif pos_x == 2 and pos_y == 1:
+        pos_x, pos_y = Only_North()
+    elif pos_x == 2 and pos_y == 2:
+        pos_x, pos_y = South_West()
+    elif pos_x == 2 and pos_y == 3:
+        pos_x, pos_y = West_East()
+    elif pos_x == 3 and pos_y == 2:
+        pos_x, pos_y = North_South()
+    elif pos_x == 3 and pos_y == 3:
+        pos_x, pos_y = South_West()
+    else:
+        print("Victory!")
+        break
