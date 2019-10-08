a319c63f3525842b6d31244132fdde39458943ee
Ulfur_Orn <ulfurinn@gmail.com>
2019-10-08 09:08:36 +0000

    Added print function for possible dir

--- TileTraveller.py
+++ TileTraveller.py
@@ -18,6 +18,21 @@ position = 1
 # Adding or subtracting 3 to the position is equivalent to moving right or left respectively.
 # If an invalid input is entered the function is run again and the user is asked for another input. (Recursion)
 
+def print_available_dir(available_dir):
+    print("You can travel:", end=" ")
+    for c in available_dir:
+        if c == "n":
+            print("(N)orth", end=" ")
+        elif c == "e":
+            print("(E)ast", end=" ")
+        elif c == "s":
+            print("(S)outh", end=" ")
+        elif c == "w":
+            print("(W)est", end=" ")
+        if c != available_dir[-1]:
+            print("or", end=" ")
+    print("")
+
 def Only_North(direction):
     if direction == "n": 
         return position + 1 
@@ -79,37 +94,38 @@ def direction_input():
 # These if-else statements check the position and run their respective function.
 while True:
     if position == 1:
-        print("You can travel: (N)orth.")
+        print_available_dir("n")
         position = Only_North(direction_input())
 
     elif position == 2:
-        print("You can travel: (N)orth or (E)ast or (S)outh.")
+        print_available_dir("nes")
         position = North_East_South(direction_input())
 
     elif position == 3:
-        print("You can travel: (E)ast or (S)outh.")
+        print_available_dir("es")
         position = South_East(direction_input())
 
     elif position == 4:
-        print("You can travel: (N)orth.")
+        print_available_dir("n")
         position = Only_North(direction_input())
 
     elif position == 5:
-        print("You can travel: (S)outh or (W)est.")
+        print_available_dir("sw")
         position = South_West(direction_input())
 
     elif position == 6:
-        print("You can travel: (E)ast or (W)est.")
+        print_available_dir("ew")
         position = West_East(direction_input())
 
     elif position == 8:
-        print("You can travel: (N)orth or (S)outh.")
+        print_available_dir("ns")
         position = North_South(direction_input())
 
     elif position == 9:
-        print("You can travel: (S)outh or (W)est.")
+        print_available_dir("sw")
         position = South_West(direction_input())
 
     else: #position == 7 # the only other possible position is 7
         print("Victory!")
+
         break # Victory has been achieved and the loop is terminated.
